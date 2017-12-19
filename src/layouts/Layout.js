9f46565711c70e66f24c37b624011ef3f9f50e42
Steve Szyszkiewicz <sszyszkiewicz@linode.com>
2017-12-19 13:35:33 -0500

    merge conflict got through into develop

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -49,7 +49,6 @@ export default class Layout extends Component {
               </span>
             </Link>
           </div>
-          <span className="MainHeader-title">Linode API v4 Documentation</span>
           <span className="MainHeader-title">Linode API v4 Documentation (Early Access)</span>
           <button
             className="ToggleNav navbar-toggler navbar-toggler-right collapsed"
