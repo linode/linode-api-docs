baa347431ca7bcb48d5fa67c07eae252ae28ed57
Jared Kobos <jaredkobos@gmail.com>
2019-01-18 11:11:19 -0500

    Remove requiredPropsFirst argument to temporarily resolve schema field ordering bug

--- build-docs.sh
+++ build-docs.sh
@@ -22,7 +22,7 @@ fi
 # run the static site builder
 chmod +x ReDoc-customized/ReDoc-customized/cli/index.js
 
-./ReDoc-customized/ReDoc-customized/cli/index.js bundle openapi.yaml --options.hideDownloadButton=true --options.pathInMiddlePanel=true --options.requiredPropsFirst=true --options.expandResponses="200," --options.noAutoAuth=true --title='Linode API Documentation'
+./ReDoc-customized/ReDoc-customized/cli/index.js bundle openapi.yaml --options.pathInMiddlePanel=true --options.requiredPropsFirst=true --options.expandResponses="200," --options.noAutoAuth=true --title='Linode API Documentation'
 
 rm index.html
 mv redoc-static.html index.html
