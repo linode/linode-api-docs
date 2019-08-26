608ab5715ea7a592ae88d6502872de2cf1581032
Jared Kobos <jaredkobos@gmail.com>
2019-08-26 13:43:56 -0400

    Update the script to replace existing version numbers rather than DEVELOPMENT

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,15 @@ if [ -z ${1+x} ]
 
   else echo "Deploying version ${1}"
 fi
+
+echo "Checking out development branch"
 git checkout development
+echo "Creating a release branch release-${1}"
 git checkout -b release-{1}
-sed -i -- "s|version: DEVELOPMENT|version: ${1}|" openapi.yaml
+echo "Updating openapi.yaml with the new version number"
+sed -ie -E -- "s|version: [0-9]+\.[0-9]+\.[0-9]|version: ${1}|" openapi.yaml
+echo "Committing the version bump"
 git add --all
 git commit -m "Bump version to ${1}"
+echo "Tagging the version commit with the version number"
 git tag ${1}
