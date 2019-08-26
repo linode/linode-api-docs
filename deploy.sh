8d00fb611c2c0aba62b8a044a32f0074a8e74ffd
Jared Kobos <jaredkobos@gmail.com>
2019-08-26 11:31:15 -0400

    Add checkout logic

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ if [ -z ${1+x} ]
 
   else echo "Deploying version ${1}"
 fi
-
+git checkout development
+git checkout -b release-{1}
 sed -i -- "s|version: DEVELOPMENT|version: ${1}|" openapi.yaml
 git add --all
 git commit -m "Bump version to ${1}"
